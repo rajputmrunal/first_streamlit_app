040cf0c007187195aaade41061f491400534ec38
rajputmrunal <126322198+rajputmrunal@users.noreply.github.com>
2023-02-25 15:36:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ streamlit.dataframe(my_fruit_list)
 
 # Display the table on the page.
 
-my_fruit_list = my_fruit_list.set_index('Fruit')
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
