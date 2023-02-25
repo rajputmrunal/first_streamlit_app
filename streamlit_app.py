3162cfc62d14bc4b7712a6026004ecb45794fc88
rajputmrunal <126322198+rajputmrunal@users.noreply.github.com>
2023-02-25 15:20:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title("my parents new healthy dinner")
+streamlit.header("Breakfast menu")
+stramlit.text("omega 3 & oatmeal")
+stramlit.text("kale,spinach & rocket smothie")
+stramlit.text("Hard-boiled free-range egg")
