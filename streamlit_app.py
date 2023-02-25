5b3d25069d32351e14c73c401dd38c71db54470a
rajputmrunal <126322198+rajputmrunal@users.noreply.github.com>
2023-02-25 15:22:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title("my parents new healthy dinner")
 streamlit.header("Breakfast menu")
 streamlit.text("omega 3 & oatmeal")
-straemlit.text("kale,spinach & rocket smothie")
+streamlit.text("kale,spinach & rocket smothie")
 streamlit.text("Hard-boiled free-range egg")
